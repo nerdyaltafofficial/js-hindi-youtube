602ecafd0edd49bff4e8ef28903c1c6c15956801
nerdyaltafofficial <63551761+nerdyaltafofficial@users.noreply.github.com>
2023-11-08 15:50:25 +0000

    data types completed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+
+let name = "hitesh";
+let age = 22;
+let isLoggedIn = true;
+
+// 1. String  = "Hello "
+// 2. Number = 3802
+// 3. Bigint = use for more numbers 
+// 4. Boolean = true or false
+// 5. Undefined = value not assigned 
+// 6. Null = standalone value
+// 7. Symbol = unique
+
+
+// 8. object = 
+
+console.log(typeof Object); //  function type
+console.log(typeof object); // undefined
+console.log(typeof null); //  object type
+console.log(typeof undefined); // undefined
+
