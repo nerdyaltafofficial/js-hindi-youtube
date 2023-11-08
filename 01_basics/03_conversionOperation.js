2078b4ce3fc5359bb77497da7a8078e4a8aa9350
nerdyaltafofficial <63551761+nerdyaltafofficial@users.noreply.github.com>
2023-11-08 16:39:22 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = null;
+
+
+// console.log(typeof(score));
+
+let intNum = Number(score);
+
+// console.log(typeof intNum);
+// console.log(intNum);
+
+//  >>Convert into Number
+// "22"  = 22
+// "22ava" = NaN
+// true = 1, false = 0;
+// null = 0;
+ 
+let isLoggedIn ;
+let isBoolean = Boolean(isLoggedIn);
+
+// console.log(isBoolean);
+
+// "" = false
+// "Ali" = true
+// " " = true
+
+
+let someNum = 33;
+
+let stringNum = String(someNum);
+
+console.log(stringNum);
+
+console.log(typeof stringNum);
