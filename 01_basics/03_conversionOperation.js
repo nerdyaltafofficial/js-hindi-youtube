625499bf4710ab1cc446223b3a47f6938589f0e6
nerdyaltafofficial <63551761+nerdyaltafofficial@users.noreply.github.com>
2023-11-09 10:09:59 +0000

    Conversion operation is done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,6 +28,60 @@ let someNum = 33;
 
 let stringNum = String(someNum);
 
-console.log(stringNum);
+// console.log(stringNum);
 
-console.log(typeof stringNum);
+// console.log(typeof stringNum);
+
+
+
+// ******************************* Operations ***********************************
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2/2);
+// console.log(3**2);
+// console.log(2%2);
+
+
+let str1 = "Hello";
+let str2 = " Altaf";
+
+let str3 = str1 * str2;
+
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 +"2");  // 12
+// console.log("1" + "2"); // 12
+
+// console.log("1" + 2 + 2);  //122
+// console.log(2 + 2 + "1");  //41
+
+// console.log( 3 + 4 * 5 % 3); // 5   read about operator precedence
+
+
+// console.log(true);  // its print true because javascript knows this is boolean value
+// console log is javascrip function which is used for output
+
+// console.log(+true);   
+
+// console.log(true);
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+// console.log(num1, num2, num3);
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter); //  101 , read about prefix & postfix
+
+
