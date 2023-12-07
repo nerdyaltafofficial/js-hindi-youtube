45219be19842258296eae7afc7925ba4dc01685c
nerdyaltafofficial <63551761+nerdyaltafofficial@users.noreply.github.com>
2023-12-07 06:51:52 +0000

    maths and  number

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,34 @@
+const score = 40;
+// console.log(score);
+
+const balance = new Number(100);
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 221.8934;
+
+// console.log(otherNumber.toPrecision(3)); // precision method is used to get round off of number
+
+const hundred = 10000000;
+
+// console.log(hundred.toLocaleString("en-IN")); // used to make string and "en-IN" is used for indian number system
+
+//+++++++++++++++++++++++ Maths +++++++++++++++++++++//
+
+// console.log(Math);
+// console.log(Math.abs(+4)); // to remove sign 
+// console.log(Math.abs(-4));   // ,,
+// console.log(Math.round(5.8)); //  used to round off the number
+// console.log(Math.ceil(6.1)); //used to upper limit number
+// console.log(Math.floor(7.8)); //used to lower limit number
+
+// console.log(Math.min(2,2,4,3,34,34,2));
+
+// console.log(Math.min(2,45,54,2,632,32,53,32,0));
+
+console.log(Math.random()); // random number shows b/w 0 and 1
+
+// Practise more about math.random and also numbers
