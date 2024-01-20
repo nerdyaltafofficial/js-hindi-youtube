f250fbc7bf25f7bd34c245cb82cc9545e0a98efd
nerdyaltafofficial <nerdyaltaf.official@gmail.com>
2024-01-20 13:05:01 +0530

    Must practise arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,24 @@
+// Arrays
+
+const myArr = [0, 1, 2, 3, 4, 5];
+
+// const myArr2 = new Array(1, 2, 3, 4, 5);
+
+// const newArr=myArr.forEach((ele) => ele * 2)
+
+// myArr.push(3,4);
+
+// myArr.shift();
+// myArr.shift();
+// myArr.shift();
+// myArr.shift();   Remove all elements from starting
+
+// myArr.unshift(0, 1, 2, 3);
+
+// console.log(myArr);
+
+const nArr = [2, 3, 4, 5, 24, 34, 234];
+
+nArr.splice(2, 2, "one", "two");
+
+console.log(nArr);
